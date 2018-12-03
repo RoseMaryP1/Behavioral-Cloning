8513eb7917f16cebe90e12cc8078d163644626db
RoseMaryP1 <42755607+RoseMaryP1@users.noreply.github.com>
2018-12-03 05:48:03 +0530

    Updated

--- model.py
+++ model.py
@@ -1,279 +1,143 @@
-import os
-import pandas as pd
-import numpy as np
-from scipy import signal
+import csv
 import cv2
-import math
-import tensorflow as tf
-tf.python.control_flow_ops = tf
+import numpy as np
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation,Lambda
-from keras.layers import Input, ELU
-from keras.optimizers import SGD, Adam, RMSprop
-from keras.utils import np_utils
-from keras.layers import Convolution2D, MaxPooling2D, Flatten
-from keras import initializations
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 
-from pathlib import Path
-import json
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
-pr_threshold = 1
-new_size_col = 64
-new_size_row = 64
+#one is newer
+#two is older
+#three is PS4 controller
+data_version = '2'
+image_path = './data-' + data_version + '/IMG/'
+angle_adjustment = 0.1
 
-def butter_lowpass(x,fcut,f_sample,order,plen): 
-    # x: unfilteted data
-    # fcut : cutoff frequency
-    # f_sample : sampling frequency
-    # order : Order of filter (usually 4)
-    # plen: padding length (typically left as 0)
-    
-    rat = fcut/f_sample
+total_left_angles = 0
+total_right_angles = 0
+total_straight_angles = 0
 
-    b, a = signal.butter(order, rat)
-    y = signal.filtfilt(b, a, x, padlen=plen)
-    return y
+#one is newer
+#two is older
+#three is PS4 controller
+images = []
+angles = []
+with open('./data-' + data_version + '/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        center_image = cv2.imread(image_path + line[0].split('/')[-1])
+        center_image_rgb = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+        images.append(center_image_rgb)
+        angles.append(float(line[3]))
+        #flipped
+        images.append(cv2.flip(center_image_rgb, 1))
+        angles.append(-float(line[3]))
 
-def moving_average(a, n=3):
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+        left_image = cv2.imread(image_path + line[1].split('/')[-1])
+        left_image_rgb = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+        images.append(left_image_rgb)
+        angles.append(float(line[3])+angle_adjustment)
+        #flipped
+        images.append(cv2.flip(left_image_rgb, 1))
+        angles.append(-(float(line[3])+angle_adjustment))
 
-#Preprocess incoming images 
-def process_newimage_file(name):
-    image = cv2.imread(name)
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    image = image/255.-.5
-    return image
+        right_image = cv2.imread(image_path + line[2].split('/')[-1])
+        right_image_rgb = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+        images.append(right_image_rgb)
+        angles.append(float(line[3])-angle_adjustment)
+        #flipped
+        images.append(cv2.flip(right_image_rgb, 1))
+        angles.append(-(float(line[3])-angle_adjustment))
 
-def augment_brightness_camera_images(image):
-    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
-    random_bright = .25+np.random.uniform()
-    image1[:,:,2] = image1[:,:,2]*random_bright
-    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
-    return image1
+        if(float(line[3]) < -0.15):
+            total_left_angles += 1
+        elif(float(line[3]) > 0.15):
+            total_right_angles += 1
+        else:
+            total_straight_angles += 1
 
+# data_samples = np.array(data_samples)
 
-def trans_image(image,steer,trans_range):
-    tr_x = trans_range*np.random.uniform()-trans_range/2
-    steer_ang = steer + tr_x/trans_range*2*.2
-    tr_y = 10*np.random.uniform()-10/2
-    Trans_M = np.float32([[1,0,tr_x],[0,1,tr_y]])
-    image_tr = cv2.warpAffine(image,Trans_M,(cols,rows))
-    
-    return image_tr,steer_ang,tr_x
+left_to_straight_ratio = total_straight_angles/total_left_angles
+right_to_straight_ratio = total_straight_angles/total_right_angles
 
-#preprocess image
-def preprocessImage(image):
-    shape = image.shape
-    image = image[math.floor(shape[0]/4):shape[0]-25, 0:shape[1]]
-    image = cv2.resize(image,(new_size_col,new_size_row), interpolation=cv2.INTER_AREA)    
-    return image 
+print('Total Samples : ', len(images))
+# print('Total Images : ', len(lines)*3)
+# print('Data Shape : ', data_samples.shape)
 
-#augment preprocessed files
-def preprocess_image_file_train(line_data):
-    i_lrc = np.random.randint(3)
-    if (i_lrc == 0):
-        path_file = line_data['left'][0].strip()
-        shift_ang = .25
-    if (i_lrc == 1):
-        path_file = line_data['center'][0].strip()
-        shift_ang = 0.
-    if (i_lrc == 2):
-        path_file = line_data['right'][0].strip()
-        shift_ang = -.25
-    y_steer = line_data['steer_sm'][0] + shift_ang
-    image = cv2.imread(path_file)
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    image,y_steer,tr_x = trans_image(image,y_steer,150)
-    image = augment_brightness_camera_images(image)
-    image = preprocessImage(image)
-    image = np.array(image)
-    ind_flip = np.random.randint(2)
-    if ind_flip==0:
-        image = cv2.flip(image,1)
-        y_steer = -y_steer
-    
-    return image,y_steer
+print()
+print('Initial Angle Distribution')
+print('Total Left Angles : ', total_left_angles)
+print('Total Right Angles : ', total_right_angles)
+print('Total Straight Angles : ', total_straight_angles)
+print('Left to Straight Ratio : ', left_to_straight_ratio)
+print('Right to Straight Ratio : ', right_to_straight_ratio)
 
-# Preprocessing Prediction files and augmenting
-def preprocess_image_file_predict(line_data):
-    path_file = line_data['center'][0].strip()
-    image = cv2.imread(path_file)
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    image = preprocessImage(image)
-    image = np.array(image)
-    return image
+# print()
+#was 'center','left','right','angle','power','brake'
+#now image array, angle array
+# print('Preview : ', data_samples[0])
 
-#Validation Generator
-def generate_valid_from_PD(data):
-    while 1:
-        for i_line in range(len(data)):
-            line_data = data.iloc[[i_line]].reset_index()
-            #print(line_data)
-            x = preprocess_image_file_predict(data)
-            x = x.reshape(1, x.shape[0], x.shape[1], x.shape[2])
-            y = line_data['steer_sm'][0]
-            y = np.array([[y]])
-            yield x, y
+X_train, X_val, y_train, y_val = train_test_split(images, angles, test_size=0.1)
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+X_val = np.array(X_val)
+y_val = np.array(y_val)
 
-#write our model to file
-def save_model(fileModelJSON,fileWeights):
-    if Path(fileModelJSON).is_file():
-        os.remove(fileModelJSON)
-    json_string = model.to_json()
-    with open(fileModelJSON,'w' ) as f:
-        json.dump(json_string, f)
-    if Path(fileWeights).is_file():
-        os.remove(fileWeights)
-    model.save_weights(fileWeights)
+train_samples_size = len(X_train)
+validation_samples_size = len(X_val)
 
-# Generator for keras training, with subsampling  
-def generate_train_from_PD_batch(data,batch_size = 32):
-    batch_images = np.zeros((batch_size, new_size_row, new_size_col, 3))
-    batch_steering = np.zeros(batch_size)
-    while 1:
-        for i_batch in range(batch_size):
-            i_line = np.random.randint(len(data))
-            line_data = data.iloc[[i_line]].reset_index()
-            
-            keep_pr = 0
-            while keep_pr == 0:
-                x,y = preprocess_image_file_train(line_data)
-                pr_unif = np.random
-                if abs(y)<.15:
-                    pr_val = np.random.uniform()
-                    if pr_val>pr_threshold:
-                        keep_pr = 1
-                else:
-                    keep_pr = 1
-            
-            batch_images[i_batch] = x
-            batch_steering[i_batch] = y
-        yield batch_images, batch_steering
+total_left_angles = 0
+total_right_angles = 0
+total_straight_angles = 0
 
-def get_model():
-    input_shape = (new_size_row, new_size_col, 3)
-    filter_size = 3
-    pool_size = (2,2)
-    model = Sequential()
-    model.add(Lambda(lambda x: x/255.-0.5,input_shape=input_shape))
-    model.add(Convolution2D(3,1,1,
-                        border_mode='valid',
-                        name='conv0', init='he_normal'))
-    model.add(Convolution2D(32,filter_size,filter_size,
-                        border_mode='valid',
-                        name='conv1', init='he_normal'))
-    model.add(ELU())
-    model.add(Convolution2D(32,filter_size,filter_size,
-                        border_mode='valid',
-                        name='conv2', init='he_normal'))
-    model.add(ELU())
-    model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(64,filter_size,filter_size,
-                        border_mode='valid',
-                        name='conv3', init='he_normal'))
-    model.add(ELU())
+for train_sample in y_train:
+    if(float(train_sample) < -0.15):
+        total_left_angles += 1
+    elif(float(train_sample) > 0.15):
+        total_right_angles += 1
+    else:
+        total_straight_angles += 1
 
-    model.add(Convolution2D(64,filter_size,filter_size,
-                        border_mode='valid',
-                        name='conv4', init='he_normal'))
-    model.add(ELU())
-    model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(128,filter_size,filter_size,
-                        border_mode='valid',
-                        name='conv5', init='he_normal'))
-    model.add(ELU())
-    model.add(Convolution2D(128,filter_size,filter_size,
-                        border_mode='valid',
-                        name='conv6', init='he_normal'))
-    model.add(ELU())
-    model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(512,name='hidden1', init='he_normal'))
-    model.add(ELU())
-    model.add(Dropout(0.5))
-    model.add(Dense(64,name='hidden2', init='he_normal'))
-    model.add(ELU())
-    model.add(Dropout(0.5))
-    model.add(Dense(16,name='hidden3',init='he_normal'))
-    model.add(ELU())
-    model.add(Dropout(0.5))
-    model.add(Dense(1, name='output', init='he_normal'))
-    return model
-        
+left_to_straight_ratio = 0
+right_to_straight_ratio = 0
 
+left_to_straight_ratio = total_straight_angles/total_left_angles
+right_to_straight_ratio = total_straight_angles/total_right_angles
 
-# Load CSV data
-csv_path = 'driving_log.csv'
-data_files_s = pd.read_csv(csv_path,
-                         index_col = False)
-data_files_s['direction'] = pd.Series('s', index=data_files_s.index)
-data_files_s.columns = ['center', 'left', 'right', 'steer', 'throttle', 'brake', 'speed','direction']
+print()
+print('Train Sample Size : ', train_samples_size)
+print('Validation Sample Size : ', validation_samples_size)
 
-# Smoothe out steering data
-rev_steer_s = np.array(data_files_s.steer,dtype=np.float32)
+print()
+print('After TTS, Angle Distribution')
+print('Total Left Angles : ', total_left_angles)
+print('Total Right Angles : ', total_right_angles)
+print('Total Straight Angles : ', total_straight_angles)
+print('Left to Straight Ratio : ', left_to_straight_ratio)
+print('Right to Straight Ratio : ', right_to_straight_ratio)
 
-t_s = np.arange(len(rev_steer_s))
-x_s = np.array(data_files_s.steer)
-y_s = rev_steer_s
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(Dropout(0.8))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
 
-steer_sm_s = rev_steer_s
-data_files_s['steer_sm'] = pd.Series(steer_sm_s, index=data_files_s.index)
+model.summary()
 
-# remove data with throttle less than .25
-ind = data_files_s['throttle']>.25
-data_files_s= data_files_s[ind].reset_index()
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, validation_data = (X_val, y_val), nb_epoch=10, shuffle=True) #, shuffle=True, validation_split=0.1
 
-
-image_c = process_newimage_file(data_files_s['center'][0].strip())
-rows,cols,channels = image_c.shape
-
-
-model = get_model()
-adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-model.compile(optimizer=adam,
-          loss='mse')
-
-## Training Cycle
-### pr_threshold reduced over training to include more small angles
-valid_s_generator = generate_valid_from_PD(data_files_s)
-
-val_size = len(data_files_s)
-pr_threshold = 1
-
-batch_size = 256
-
-i_best = 0
-val_best = 1000
-
-for i_pr in range(10):
-
-    train_r_generator = generate_train_from_PD_batch(data_files_s,batch_size)
-
-    nb_vals = np.round(len(data_files_s)/val_size)-1
-    history = model.fit_generator(train_r_generator,
-            samples_per_epoch=20224, nb_epoch=1,validation_data=valid_s_generator,
-                        nb_val_samples=val_size)
-    
-    fileModelJSON = 'model_' + str(i_pr) + '.json'
-    fileWeights = 'model_' + str(i_pr) + '.h5'
-    
-    save_model(fileModelJSON,fileWeights)
-    
-    val_loss = history.history['val_loss'][0]
-    if val_loss < val_best:
-        i_best = i_pr 
-        val_best = val_loss
-        fileModelJSON = 'model_best.json'
-        fileWeights = 'model_best.h5'
-        save_model(fileModelJSON,fileWeights)
-    
-    
-    pr_threshold = 1/(i_pr+1)
-print('Best model found at iteration # ' + str(i_best))
-print('Best Validation score : ' + str(np.round(val_best,4)))
+model.save('model-' + data_version + '.h5')
